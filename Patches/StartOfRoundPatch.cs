e33ed69830520b16e56d856ce849ab720a6a663c
Piggy <70088001+Piggy3590@users.noreply.github.com>
2023-12-15 06:24:03 +0900

    Add files via upload

---
+++ Patches/StartOfRoundPatch.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx.Logging;
+using DunGen;
+using GameNetcodeStuff;
+using HarmonyLib;
+using Mimics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MimicKoreanPatch
+{
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class StartOfRoundPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("Update")]
+        private static void Update_Postfix()
+        {
+            MimicDoor[] mimicDoors = UnityEngine.Object.FindObjectsOfType<MimicDoor>();
+            foreach (MimicDoor mimicDoor in mimicDoors)
+            {
+                if (mimicDoor.interactTrigger != null)
+                {
+                    mimicDoor.interactTrigger.hoverTip = "나가기 : [LMB]";
+                    mimicDoor.interactTrigger.holdTip = "나가기 : [LMB]";
+                }
+                else
+                {
+                    Debug.LogError(mimicDoor.name + " doesnt have InteractTrigger");
+                }
+            }
+        }
+    }
+}
+
---
+++ Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Mimics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MimicKoreanPatch
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class MimicKoreanPatchModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Piggy3590.MimicKoreanPatch";
+        private const string modName = "MimicKoreanPatch";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static MimicKoreanPatchModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Mimic Korean Patch is loaded");
+
+            harmony.PatchAll(typeof(MimicKoreanPatchModBase));
+            harmony.PatchAll(typeof(StartOfRoundPatch));
+        }
+    }
+}
